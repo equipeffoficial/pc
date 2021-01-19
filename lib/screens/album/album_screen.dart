673e8113158ee8f6331cc32b3acc089080c64824
Felipe Marques <equipeffoficial@gmail.com>
2021-01-19 14:48:11 -0400

    Editando tela de album

--- lib/screens/album/album_screen.dart
+++ lib/screens/album/album_screen.dart
@@ -12,11 +12,58 @@ class AlbumScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(album.name),
+        title: Text('Ouça Agora'),
         centerTitle: true,
       ),
       backgroundColor: Colors.white,
-      body: Container()
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 30, right: 20, left: 20),
+            child: Text(
+              '${album.name}',
+              style: TextStyle(
+                color: Colors.grey[800],
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Padding(
+            padding:  const EdgeInsets.only(top: 8, right: 20, left: 20, bottom: 25),
+            child: Text(
+              '${album.description}',
+              style: TextStyle(
+                  color: Colors.grey[800],
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Center(
+            child: ClipRRect(   
+            borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                  album.image,
+                height: 280,
+                width: 280,
+              ),
+            ),
+          ),
+          Padding(
+            padding:  const EdgeInsets.only(top: 10, right: 50, left: 20),
+            child: Text(
+              'Artista: ${album.artist}',
+              textAlign: TextAlign.end,
+              style: TextStyle(
+                color: Colors.grey[800],
+                fontSize: 13,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+        ],
+      )
     );
   }
 }
